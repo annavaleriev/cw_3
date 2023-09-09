4b0de44bc2af78b4e92c757ebb724237c3d27e6f
annavaleriev <filenko.a@gmail.com>
2023-09-09 14:51:08 +0300

    создан тестовый файл test_operations.json и помещен  в папку tests для тестирования data в settings.py добавлен пусть до тестового файла test_operations.json

--- settings.py
+++ settings.py
@@ -3,3 +3,8 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent
 DATA_PATH = Path.joinpath(ROOT_PATH, 'data')
 OPERATION_JSON = Path.joinpath(DATA_PATH, 'operations.json')
+
+
+ROOT_PATH_TEST = Path(__file__).parent
+DATA_PATH_TEST = Path.joinpath(ROOT_PATH_TEST, 'tests')
+OPERATION_JSON_TEST = Path.joinpath(DATA_PATH_TEST, 'test_operations.json')
