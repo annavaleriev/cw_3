6c4df53382941db18e4b9527d84e091f628b8d4c
annavaleriev <filenko.a@gmail.com>
2023-09-10 19:18:58 +0300

    изменен тест def test_get_executed_operations исправлен пусть к файлу в settings.py

--- settings.py
+++ settings.py
@@ -5,6 +5,5 @@ DATA_PATH = Path.joinpath(ROOT_PATH, 'data')
 OPERATION_JSON = Path.joinpath(DATA_PATH, 'operations.json')
 
 
-ROOT_PATH_TEST = Path(__file__).parent
-DATA_PATH_TEST = Path.joinpath(ROOT_PATH_TEST, 'tests')
+DATA_PATH_TEST = Path.joinpath(ROOT_PATH, 'tests')
 OPERATION_JSON_TEST = Path.joinpath(DATA_PATH_TEST, 'test_operations.json')
--- tests/test_data.py
+++ tests/test_data.py
@@ -2,10 +2,12 @@ from classes.data import DataProcessor
 from tests.conftest import executed_operations, sorted_operations, last_five__operations
 
 
-def test_get_executed_operations(test_json, executed_operations):
-    test_get_executed_operations = DataProcessor(test_json)
-    # assert DataProcessor.get_executed_operations(test_get_executed_operations, test_json) == []
-    assert DataProcessor.get_executed_operations(test_get_executed_operations, test_json) == executed_operations
+def test_get_executed_operations():
+    data_processor = DataProcessor('')
+    assert data_processor.get_executed_operations([{"state": "EXECUTED", "id": 1}, {"state": "CANCELED", "id": 2}]) == [
+        {"state": "EXECUTED", "id": 1}]
+    assert data_processor.get_executed_operations([]) == []
+    assert data_processor.get_executed_operations([{"id": 1}, {"id": 2}]) == []
 
 
 def test_sort_operations(executed_operations, sorted_operations):
@@ -16,6 +18,3 @@ def test_sort_operations(executed_operations, sorted_operations):
 def test_get_five_operations(sorted_operations, last_five__operations):
     test_get_executed_operations = DataProcessor(sorted_operations)
     assert DataProcessor.get_five_operations(test_get_executed_operations, sorted_operations) == last_five__operations
-
-
-
